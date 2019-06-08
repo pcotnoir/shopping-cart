4cac091eb218783387b875b47576c3d264ac3d89
pcotnoir <pc2508@stern.nyu.edu>
2019-06-08 18:16:56 -0400

    Date and time output

--- shopping_cart.py
+++ shopping_cart.py
@@ -73,6 +73,9 @@ print("Web: www.cotnoirgroceries.com")
 print("Phone: 1.800.675.7245")
 
 # The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
+import time
+print("Checkout time: ", time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"))
+
 # The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
 # The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
 # The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
