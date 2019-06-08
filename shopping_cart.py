6493266e300532faaa9c7d1925cf0feb0c3d8c86
pcotnoir <pc2508@stern.nyu.edu>
2019-06-08 18:22:56 -0400

    Display shopping cart items receipt output

--- shopping_cart.py
+++ shopping_cart.py
@@ -75,8 +75,19 @@ print("Phone: 1.800.675.7245")
 # The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
 import time
 print("Checkout time: ", time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"))
+print("-----------------------------------") 
 
 # The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
+
+print("Shopping Cart Items: ")
+
+for selected_id in selected_ids:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+        print("+ " + matching_product["name"] + " " + "($" + str(matching_product["price"]) + ")")
+print("-----------------------------------") 
+
 # The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
 # The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 # The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
